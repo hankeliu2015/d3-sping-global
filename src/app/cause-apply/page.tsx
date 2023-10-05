cc7a9734abdd45b865664759a9c1f081efa50cd5
Will Weiss <will.weiss1230@gmail.com>
2023-10-05 15:30:13 -0400

    form fix

--- src/app/cause-apply/page.tsx
+++ src/app/cause-apply/page.tsx
@@ -39,100 +39,95 @@ import {
               <div className="sm:col-span-4">
                 <div>
                   <label
-                    htmlFor="email"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Cause Name
-                  </label>
-                  <div className="mt-2 flex rounded-md shadow-sm">
-                    <div className="relative flex flex-grow items-stretch focus-within:z-10">
-                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                        <FlagIcon
-                          className="h-5 w-5 text-gray-400"
-                          aria-hidden="true"
+                    Cause Name*
+                    <div className="mt-2 flex rounded-md shadow-sm">
+                      <div className="relative flex flex-grow items-stretch focus-within:z-10">
+                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                          <FlagIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        </div>
+                        <input
+                          name="cause-name"
+                          className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="Name of your cause"
+                          required
                         />
                       </div>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Name of your cause"
-                      />
                     </div>
-                  </div>
+                  </label>
                 </div>
               </div>
   
               <div className="sm:col-span-4 pr-4 ">
                 <label
-                  htmlFor="country"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Country
+                  Country*
+                  <div className="mt-2">
+                    <select
+                      id="country-input"
+                      name="country"
+                      autoComplete="country-name"
+                      className="block w-full rounded-md border-0 pl-3 py-1.5 bg-center text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      required
+                    >
+                      <option>{getUnicodeFlagIcon("US")} United States </option>
+                      {!isClient ? '' : countrySelectOptions()}
+                    </select>
+                  </div>
                 </label>
-                <div className="mt-2">
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country-name"
-                    className="block w-full rounded-md border-0 pl-3 py-1.5 bg-center text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                  >
-                    <option>{getUnicodeFlagIcon("US")} United States </option>
-                    {!isClient ? '' : countrySelectOptions()}
-                  </select>
-                </div>
               </div>
   
               <div className="sm:col-span-4">
                 <label
-                  htmlFor="mission"
                   className="inline-block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Mission
+                  Mission*
+                  <a href="https://www.youtube.com/watch?v=BPdxppjKCwQ" target="blank" className="mt-3 pl-2 text-sm leading-6 text-blue-600 hover:text-violet-600">(8 words or less)</a>
+                    <div className="mt-2 relative flex flex-grow items-stretch focus-within:z-10">
+                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                        <InformationCircleIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <input
+                        name="misson"
+                        className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="mission"
+                      />
+                    </div>
                 </label>
-                <a href="https://www.youtube.com/watch?v=BPdxppjKCwQ" target="blank"className="mt-3 pl-2 text-sm leading-6 text-gray-600 hover:text-violet-600">(8 words or less)</a>
-                <div className="mt-2 relative flex flex-grow items-stretch focus-within:z-10">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <InformationCircleIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
+                
+              </div>
+  
+              <div className="col-span-full">
+                <label
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  How can digital technology have a huge impact in this problem area?*
+                  <div className="mt-2">
+                    <textarea
+                      name="about"
+                      rows={3}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                      defaultValue={""}
+                      required
                     />
                   </div>
-                  <input
-                    type='text'
-                    id="about"
-                    name="misson"
-                    className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="mission"
-                  />
-                </div>
-              </div>
-  
-              <div className="col-span-full">
-                <label
-                  htmlFor="about"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  How can digital technology have a huge impact in this problem area?
                 </label>
-                <div className="mt-2">
-                  <textarea
-                    id="about"
-                    name="about"
-                    rows={3}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue={""}
-                  />
-                </div>
+                
               </div>
   
               <div className="col-span-full">
                 <label
-                  htmlFor="file-upload"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Supporting Documents(Optional)
+                  Supporting Documents
                 </label>
                 <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                   <div className="text-center">
@@ -147,7 +142,6 @@ import {
                       >
                         <span>Upload a file</span>
                         <input
-                          id="file-upload"
                           name="file-upload"
                           type="file"
                           className="sr-only"
@@ -169,30 +163,28 @@ import {
             <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-4">
                 <label
-                  htmlFor="first-name"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Your Name
+                  Your Name*
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="first-name"
+                      autoComplete="name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      required
+                    />
+                  </div>
                 </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                
               </div>
   
               <div className="sm:col-span-4">
-                {/* input field with leading icon  */}
                 <div>
                   <label
-                    htmlFor="email"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Your Email
+                    Your Email*
                   </label>
                   <div className="relative mt-2 rounded-md shadow-sm">
                     <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -204,103 +196,12 @@ import {
                     <input
                       type="email"
                       name="email"
-                      id="email"
                       className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       placeholder="you@example.com"
+                      autoComplete="email"
+                      required
                     />
                   </div>
-                </div>
-              </div>
-  
-              <div className="sm:col-span-4">
-                <label
-                  htmlFor="country"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Country
-                </label>
-                <div className="mt-2">
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country-name"
-                    className="block w-full rounded-md border-0 pl-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                  >
-                    <option>{getUnicodeFlagIcon("US")} United States </option>
-                    {!isClient ? '' : countrySelectOptions()}
-                  </select>
-                </div>
-              </div>
-  
-              {/* <div className="col-span-2">
-                <label
-                  htmlFor="street-address"
-                  className="inline-block text-sm font-medium leading-6 text-gray-900 whitespace-nowrap"
-                >
-                  Street address (Optional)
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="street-address"
-                    id="street-address"
-                    autoComplete="street-address"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div> */}
-  
-              <div className="sm:col-span-1 sm:col-start-1">
-                <label
-                  htmlFor="city"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  City
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    autoComplete="address-level2"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div className="sm:col-span-1">
-                <label
-                  htmlFor="region"
-                  className="block text-sm font-medium leading-6 text-gray-900 whitespace-nowrap"
-                >
-                  State / Province
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="region"
-                    id="region"
-                    autoComplete="address-level1"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div className="sm:col-span-1">
-                <label
-                  htmlFor="postal-code"
-                  className="block text-sm font-medium leading-6 text-gray-900 whitespace-nowrap"
-                >
-                  ZIP / Postal code
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="postal-code"
-                    id="postal-code"
-                    autoComplete="postal-code"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
                 </div>
               </div>
             </div>
