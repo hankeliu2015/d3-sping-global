57e116daa29d2882584709ab7c925c1788b5553b
Hank-Liu <hanke.liu@gmail.com>
2023-11-03 11:06:33 -0400

    restructure thank-you page layout

--- src/app/thank-you/layout.tsx
+++ src/app/thank-you/layout.tsx
@@ -1,7 +1,14 @@
-export default function DashboardLayout({
+import { Layout as SharedLayout } from '@/components/shared/layout'
+// import SEO from '@/components/shared/seo'
+
+export default function PageLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
-  return children
-}
+  return (
+    <SharedLayout additionalClassNames="thank-you">
+      {/* <SEO pageTitle="Thank You" /> */}
+      {children}
+    </SharedLayout>
+)}
--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 // import { Card, CardContent } from '@material-ui/core'
 import Hero from '@/components/shared/hero'
-import { Layout } from '@/components/shared/layout'
-// import SEO from '@/components/shared/seo'
 
 export default function ThankYouPage(): JSX.Element {
   return (
-    <Layout additionalClassNames="thank-you">
-      {/* <SEO pageTitle="Thank You" /> */}
+    <>
       <Hero additionalClassNames="thank-you">
         {/* <Card> */}
         <h2>
@@ -16,6 +13,6 @@ export default function ThankYouPage(): JSX.Element {
         {/* <CardContent className="thank-you">{children}</CardContent> */}
         {/* </Card> */}
       </Hero>
-    </Layout>
+    </>
   )
 }
