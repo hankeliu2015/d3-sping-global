ccfaa9613a42bf7092187657f24ceda0f764144e
Hank-Liu <hanke.liu@gmail.com>
2023-11-04 22:24:28 -0400

    fix cause page-layout type error during deployment

--- src/app/cause/layout.tsx
+++ src/app/cause/layout.tsx
@@ -1,1 +1,12 @@
-export { Layout as default } from '@/components/shared/layout';
+import { Layout as SharedLayout } from '@/components/shared/layout'
+
+export default function PageLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <SharedLayout>
+      {children}
+    </SharedLayout>
+)}
